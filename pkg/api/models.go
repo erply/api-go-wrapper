a75a8b583ae33bede98def75056531712597cfd5
Jake Malay <jake0malay3@gmail.com>
2020-04-29 21:03:31 +0300

    cleanup

--- pkg/api/models.go
+++ pkg/api/models.go
@@ -41,31 +41,6 @@ type CustomerDataProcessingLog struct {
 	fields       []string
 	customerIds  []int
 }
-type (
-	verifyIdentityTokenResponse struct {
-		Status Status      `json:"status"`
-		Result SessionInfo `json:"records"`
-	}
-
-	SessionInfo struct {
-		SessionKey string `json:"sessionKey"`
-	}
-
-	getIdentityTokenResponse struct {
-		Status Status        `json:"status"`
-		Result IdentityToken `json:"records"`
-	}
-	IdentityToken struct {
-		Jwt string `json:"identityToken"`
-	}
-	JwtTokenResponse struct {
-		Status  Status   `json:"status"`
-		Records JwtToken `json:"records"`
-	}
-	JwtToken struct {
-		Token string `json:"token"`
-	}
-)
 type DocumentDatas []DocumentData
 
 type DocumentData struct {
