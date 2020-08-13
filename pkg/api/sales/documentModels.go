3d05d8a5d0826fff10d264c33a24c41b8e31836d
Andrey Pozolotin <posandrey@gmail.com>
2020-08-13 20:35:43 +0200

    Added missing sales document fields

--- pkg/api/sales/documentModels.go
+++ pkg/api/sales/documentModels.go
@@ -4,6 +4,19 @@ import (
 	sharedCommon "github.com/erply/api-go-wrapper/pkg/api/common"
 )
 
+const (
+	SaleDocumentTypeInvWayBill    = "INVWAYBILL"
+	SaleDocumentTypeCASHINVOICE   = "CASHINVOICE"
+	SaleDocumentTypeWayBill       = "WAYBILL"
+	SaleDocumentTypePrepayment    = "PREPAYMENT"
+	SaleDocumentTypeOffer         = "OFFER"
+	SaleDocumentTypeExportInvoice = "EXPORTINVOICE"
+	SaleDocumentTypeReservation   = "RESERVATION"
+	SaleDocumentTypeCreditInvoice = "CREDITINVOICE"
+	SaleDocumentTypeOrder         = "ORDER"
+	SaleDocumentTypeInvoice       = "INVOICE"
+)
+
 type (
 	SaleDocument struct {
 		ID           int    `json:"id"`
@@ -24,36 +37,42 @@ type (
 		//Payer if invoice_client_is_payer = 0
 		PayerID int `json:"payerID"`
 
-		AddressID               int                 `json:"addressID"`
-		PayerAddressID          int                 `json:"payerAddressID"`
-		ShipToAddressID         string              `json:"shipToAddressID"`
-		ContactID               int                 `json:"contactID"`
-		EmployeeID              int                 `json:"employeeID"`
-		PaymentDays             string              `json:"paymentDays"`
-		Confirmed               string              `json:"confirmed"`
-		Notes                   string              `json:"notes"`
-		LastModified            int                 `json:"lastModified"`
-		PackingUnitsDescription string              `json:"packingUnitsDescription"`
-		CurrencyCode            string              `json:"currencyCode"`
-		ContactName             string              `json:"contactName"`
-		Type                    string              `json:"type"`
-		InvoiceState            string              `json:"invoiceState"`
-		PaymentType             string              `json:"paymentType"`
-		BaseDocuments           []BaseDocument      `json:"baseDocuments"`
-		NetTotal                float64             `json:"netTotal"`
-		VatTotal                float64             `json:"vatTotal"`
-		VatTotalsByTaxRates     VatTotalsByTaxRates `json:"vatTotalsByTaxRate"`
-		Rounding                float64             `json:"rounding"`
-		Total                   float64             `json:"total"`
-		Paid                    string              `json:"paid"`
-		PrintDiscounts          int                 `json:"printDiscounts"`
-		ReferenceNumber         string              `json:"referenceNumber"`
-		CustomReferenceNumber   string              `json:"customReferenceNumber"`
-		PaymentStatus           string              `json:"paymentStatus"`
-		Penalty                 string              `json:"penalty"`
-		InvoiceLink             string              `json:"invoiceLink"`
-		InvoiceRows             []InvoiceRow        `json:"rows"`
-		Attributes              []PaymentAttribute  `json:"attributes"`
+		AddressID                int                 `json:"addressID"`
+		PayerAddressID           int                 `json:"payerAddressID"`
+		ShipToAddressID          string              `json:"shipToAddressID"`
+		ContactID                int                 `json:"contactID"`
+		EmployeeID               int                 `json:"employeeID"`
+		PaymentDays              string              `json:"paymentDays"`
+		Confirmed                string              `json:"confirmed"`
+		Notes                    string              `json:"notes"`
+		LastModified             int                 `json:"lastModified"`
+		PackingUnitsDescription  string              `json:"packingUnitsDescription"`
+		InventoryTransactionDate string              `json:"inventoryTransactionDate"`
+		CurrencyCode             string              `json:"currencyCode"`
+		ContactName              string              `json:"contactName"`
+		ClientName               string              `json:"clientName"`
+		Type                     string              `json:"type"`
+		InvoiceState             string              `json:"invoiceState"`
+		PaymentType              string              `json:"paymentType"`
+		BaseDocuments            []BaseDocument      `json:"baseDocuments"`
+		NetTotal                 float64             `json:"netTotal"`
+		VatTotal                 float64             `json:"vatTotal"`
+		VatTotalsByTaxRates      VatTotalsByTaxRates `json:"vatTotalsByTaxRate"`
+		Rounding                 float64             `json:"rounding"`
+		Total                    float64             `json:"total"`
+		Paid                     string              `json:"paid"`
+		PrintDiscounts           int                 `json:"printDiscounts"`
+		ReferenceNumber          string              `json:"referenceNumber"`
+		CustomReferenceNumber    string              `json:"customReferenceNumber"`
+		PaymentStatus            string              `json:"paymentStatus"`
+		Penalty                  string              `json:"penalty"`
+		InvoiceLink              string              `json:"invoiceLink"`
+		EmployeeName             string              `json:"employeeName"`
+		TransportTypeName        string              `json:"transportTypeName"`
+		ShipToName               string              `json:"shipToName"`
+		ShippingDate             string              `json:"shippingDate"`
+		InvoiceRows              []InvoiceRow        `json:"rows"`
+		Attributes               []PaymentAttribute  `json:"attributes"`
 	}
 
 	InvoiceRow struct {
