c5114088759937078235d6a7f02e45cb15ca1613
Andrey Pozolotin <posandrey@gmail.com>
2020-10-11 23:24:34 +0300

    SavePriceList AddProductToPriceList EditProductToPriceList DeleteProductsFromPriceList + bulk

--- examples/prices/main.go
+++ examples/prices/main.go
@@ -13,50 +13,58 @@ func main() {
 	apiClient, err := api.BuildClient()
 	common.Die(err)
 
-	productsInPrice, err := GetProductsInPriceList(apiClient)
-	common.Die(err)
-	fmt.Printf("GetProductsInPriceList:\n%+v\n", productsInPrice)
+	//productsInPrice, err := GetProductsInPriceList(apiClient)
+	//common.Die(err)
+	//fmt.Printf("GetProductsInPriceList:\n%+v\n", productsInPrice)
+	//
+	//bulkPrices, err := GetProductsInPriceListBulk(apiClient)
+	//common.Die(err)
+	//fmt.Printf("GetProductsInPriceListBulk:\n%+v\n", bulkPrices)
+	//
+	//supplierBulkPrices, err := GetSupplierPriceListsBulk(apiClient)
+	//common.Die(err)
+	//
+	//supplierPrices, err := GetSupplierPriceLists(apiClient, "4644")
+	//common.Die(err)
+	//
+	//fmt.Printf("BulkPrices:\n%+v\n", supplierBulkPrices)
+	//fmt.Printf("SupplierPrices:\n%+v\n", supplierPrices)
+	//
+	//bulkProductPrices, err := GetProductsInSupplierPriceListBulk(apiClient)
+	//common.Die(err)
+	//fmt.Printf("BulkProductPrices:\n%+v\n", bulkProductPrices)
+	//
+	//productPrices, err := GetProductsInSupplierPriceList(apiClient)
+	//common.Die(err)
+	//fmt.Printf("ProductPrices:\n%+v\n", productPrices)
+	//
+	//res, err := AddProductToSupplierPriceList(apiClient, "65661", "1", "100.23")
+	//common.Die(err)
+	//fmt.Printf("AddProductToSupplierPriceList:\n%+v\n", res)
+	//
+	//bulkRes, err := ChangeProductToSupplierPriceListBulk(apiClient, []string{"65659", "65660"}, []string{"1", "1"}, []string{"10.22", "111.00"})
+	//common.Die(err)
+	//fmt.Printf("ChangeProductToSupplierPriceListBulk:\n%+v\n", bulkRes)
+	//
+	//bulkResDel, err := DeleteProductsFromSupplierPriceListBulk(apiClient)
+	//common.Die(err)
+	//fmt.Printf("DeleteProductsFromSupplierPriceListBulk:\n%+v\n", bulkResDel)
+	//
+	//saveSupPriceResp, err := SaveSupplierPriceList(apiClient)
+	//common.Die(err)
+	//fmt.Printf("SaveSupplierPriceList:\n%+v\n", saveSupPriceResp)
+	//
+	//saveSupPriceRespBulk, err := SaveSupplierPriceListBulk(apiClient)
+	//common.Die(err)
+	//fmt.Printf("SaveSupplierPriceListBulk:\n%+v\n", saveSupPriceRespBulk)
 
-	bulkPrices, err := GetProductsInPriceListBulk(apiClient)
-	common.Die(err)
-	fmt.Printf("GetProductsInPriceListBulk:\n%+v\n", bulkPrices)
-
-	supplierBulkPrices, err := GetSupplierPriceListsBulk(apiClient)
-	common.Die(err)
-
-	supplierPrices, err := GetSupplierPriceLists(apiClient, "4644")
-	common.Die(err)
-
-	fmt.Printf("BulkPrices:\n%+v\n", supplierBulkPrices)
-	fmt.Printf("SupplierPrices:\n%+v\n", supplierPrices)
-
-	bulkProductPrices, err := GetProductsInSupplierPriceListBulk(apiClient)
-	common.Die(err)
-	fmt.Printf("BulkProductPrices:\n%+v\n", bulkProductPrices)
-
-	productPrices, err := GetProductsInSupplierPriceList(apiClient)
-	common.Die(err)
-	fmt.Printf("ProductPrices:\n%+v\n", productPrices)
-
-	res, err := AddProductToSupplierPriceList(apiClient, "65661", "1", "100.23")
-	common.Die(err)
-	fmt.Printf("AddProductToSupplierPriceList:\n%+v\n", res)
-
-	bulkRes, err := ChangeProductToSupplierPriceListBulk(apiClient, []string{"65659", "65660"}, []string{"1", "1"}, []string{"10.22", "111.00"})
-	common.Die(err)
-	fmt.Printf("ChangeProductToSupplierPriceListBulk:\n%+v\n", bulkRes)
-
-	bulkResDel, err := DeleteProductsFromSupplierPriceListBulk(apiClient)
-	common.Die(err)
-	fmt.Printf("DeleteProductsFromSupplierPriceListBulk:\n%+v\n", bulkResDel)
-
-	saveSupPriceResp, err := SaveSupplierPriceList(apiClient)
-	common.Die(err)
-	fmt.Printf("SaveSupplierPriceList:\n%+v\n", saveSupPriceResp)
-
-	saveSupPriceRespBulk, err := SaveSupplierPriceListBulk(apiClient)
-	common.Die(err)
-	fmt.Printf("SaveSupplierPriceListBulk:\n%+v\n", saveSupPriceRespBulk)
+	//SavePriceList(apiClient)
+	//SavePriceListBulk(apiClient)
+	//AddProductToPriceList(apiClient)
+	//EditProductToPriceList(apiClient)
+	//ChangeProductToPriceListBulk(apiClient)
+	DeleteProductsFromPriceListBulk(apiClient)
+	//DeleteProductsFromPriceList(apiClient)
 }
 
 func ChangeProductToSupplierPriceListBulk(cl *api.Client, productIds, priceIds, prices []string) (prices.ChangeProductToSupplierPriceListResponseBulk, error) {
@@ -263,4 +271,129 @@ func SaveSupplierPriceListBulk(cl *api.Client) (prices.SaveSupplierPriceListResp
 
 	resp, err := cli.SaveSupplierPriceListBulk(ctx, bulkItems, map[string]string{})
 	return resp, err
+}
+
+func SavePriceList(cl *api.Client) {
+	cli := cl.PricesManager
+
+	req := map[string]string{
+		"name":   "some price list",
+		"active": "1",
+		"type":   "BASE_PRICE_LIST",
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	resp, err := cli.SavePriceList(ctx, req)
+	common.Die(err)
+	fmt.Println(common.ConvertSourceToJsonStrIfPossible(resp))
+}
+
+func AddProductToPriceList(cl *api.Client) {
+	cli := cl.PricesManager
+
+	req := map[string]string{
+		"priceListID": "100000002",
+		"productID":   "100001020",
+		"price":       "22.2",
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	resp, err := cli.AddProductToPriceList(ctx, req)
+	common.Die(err)
+	fmt.Println(common.ConvertSourceToJsonStrIfPossible(resp))
+}
+
+func EditProductToPriceList(cl *api.Client) {
+	cli := cl.PricesManager
+
+	req := map[string]string{
+		"priceListProductID": "100001029",
+		"price":              "22.2",
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	resp, err := cli.EditProductToPriceList(ctx, req)
+	common.Die(err)
+	fmt.Println(common.ConvertSourceToJsonStrIfPossible(resp))
+}
+
+func SavePriceListBulk(cl *api.Client) {
+	cli := cl.PricesManager
+
+	bulkItems := []map[string]interface{}{
+		{
+			"pricelistID": "303",
+			"name":        "some price list",
+			"active":      "0",
+			"type":        "BASE_PRICE_LIST",
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	resp, err := cli.SavePriceListBulk(ctx, bulkItems, map[string]string{})
+	common.Die(err)
+	fmt.Println(common.ConvertSourceToJsonStrIfPossible(resp))
+}
+
+func ChangeProductToPriceListBulk(cl *api.Client) {
+	cli := cl.PricesManager
+
+	bulkItems := []map[string]interface{}{
+		{
+			"priceListProductID": "88196",
+			"price":              "22.2",
+		},
+		{
+			"priceListID": "303",
+			"productID":   "2",
+			"price":       "33.2",
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	resp, err := cli.ChangeProductToPriceListBulk(ctx, bulkItems, map[string]string{})
+	common.Die(err)
+	fmt.Println(common.ConvertSourceToJsonStrIfPossible(resp))
+}
+
+func DeleteProductsFromPriceListBulk(cl *api.Client) {
+	cli := cl.PricesManager
+
+	bulkFilters := []map[string]interface{}{
+		{
+			"priceListID":        "100000002",
+			"priceListProductIDs": "100001030",
+		},
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	resp, err := cli.DeleteProductsFromPriceListBulk(ctx, bulkFilters, map[string]string{})
+	common.Die(err)
+	fmt.Println(common.ConvertSourceToJsonStrIfPossible(resp))
+}
+
+func DeleteProductsFromPriceList(cl *api.Client) {
+	cli := cl.PricesManager
+
+	filters := map[string]string{
+		"priceListID":        "100000002",
+		"priceListProductIDs": "100001029",
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	resp, err := cli.DeleteProductsFromPriceList(ctx, filters)
+	common.Die(err)
+	fmt.Println(common.ConvertSourceToJsonStrIfPossible(resp))
 }
--- pkg/api/prices/requests.go
+++ pkg/api/prices/requests.go
@@ -407,3 +407,233 @@ func (cli *Client) SaveSupplierPriceListBulk(ctx context.Context, bulkRequest []
 
 	return bulkResp, nil
 }
+
+func (cli *Client) SavePriceList(ctx context.Context, filters map[string]string) (*SavePriceListResult, error) {
+	resp, err := cli.SendRequest(ctx, "savePriceList", filters)
+	if err != nil {
+		return nil, erro.NewFromError("savePriceList request failed", err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &SavePriceListResultResponse{}
+	if err := json.Unmarshal(body, &res); err != nil {
+		return nil, fmt.Errorf("ERPLY API: failed to unmarshal SavePriceListResultResponse from '%s': %v", string(body), err)
+	}
+
+	if !common.IsJSONResponseOK(&res.Status) {
+		return nil, erro.NewFromResponseStatus(&res.Status)
+	}
+
+	if len(res.SavePriceListResults) == 0 {
+		return nil, nil
+	}
+
+	return &res.SavePriceListResults[0], nil
+}
+
+func (cli *Client) SavePriceListBulk(ctx context.Context, bulkRequest []map[string]interface{}, baseFilters map[string]string) (SavePriceListResponseBulk, error) {
+	var bulkResp SavePriceListResponseBulk
+
+	if len(bulkRequest) > common2.MaxBulkRequestsCount {
+		return bulkResp, fmt.Errorf("cannot save more than %d price lists in one bulk request", common2.MaxBulkRequestsCount)
+	}
+
+	bulkInputs := make([]common.BulkInput, 0, len(bulkRequest))
+	for _, bulkInput := range bulkRequest {
+		bulkInputs = append(bulkInputs, common.BulkInput{
+			MethodName: "savePriceList",
+			Filters:    bulkInput,
+		})
+	}
+
+	resp, err := cli.SendRequestBulk(ctx, bulkInputs, baseFilters)
+	if err != nil {
+		return bulkResp, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return bulkResp, err
+	}
+
+	if err := json.Unmarshal(body, &bulkResp); err != nil {
+		return bulkResp, fmt.Errorf("ERPLY API: failed to unmarshal SavePriceListResponseBulk from '%s': %v", string(body), err)
+	}
+
+	if !common.IsJSONResponseOK(&bulkResp.Status) {
+		return bulkResp, erro.NewErplyError(bulkResp.Status.ErrorCode.String(), bulkResp.Status.Request+": "+bulkResp.Status.ResponseStatus)
+	}
+
+	for _, bulkRespItem := range bulkResp.BulkItems {
+		if !common.IsJSONResponseOK(&bulkRespItem.Status.Status) {
+			return bulkResp, erro.NewErplyError(
+				bulkRespItem.Status.ErrorCode.String(),
+				fmt.Sprintf("%+v", bulkRespItem.Status),
+			)
+		}
+	}
+
+	return bulkResp, nil
+}
+
+func (cli *Client) AddProductToPriceList(ctx context.Context, filters map[string]string) (*ChangeProductToPriceListResult, error) {
+	return cli.persistProductToPriceList(ctx, "addProductToPriceList", filters)
+}
+
+func (cli *Client) EditProductToPriceList(ctx context.Context, filters map[string]string) (*ChangeProductToPriceListResult, error) {
+	return cli.persistProductToPriceList(ctx, "editProductInPriceList", filters)
+}
+
+func (cli *Client) persistProductToPriceList(ctx context.Context, method string, filters map[string]string) (*ChangeProductToPriceListResult, error) {
+	resp, err := cli.SendRequest(ctx, method, filters)
+	if err != nil {
+		return nil, erro.NewFromError(method+" request failed", err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &ChangeProductToPriceListResponse{}
+	if err := json.Unmarshal(body, &res); err != nil {
+		return nil, fmt.Errorf("ERPLY API: failed to unmarshal ChangeProductToPriceListResponse from '%s': %v", string(body), err)
+	}
+
+	if !common.IsJSONResponseOK(&res.Status) {
+		return nil, erro.NewFromResponseStatus(&res.Status)
+	}
+
+	if len(res.ChangeProductToPriceListResults) == 0 {
+		return nil, nil
+	}
+
+	return &res.ChangeProductToPriceListResults[0], nil
+}
+
+func (cli *Client) ChangeProductToPriceListBulk(ctx context.Context, bulkRequest []map[string]interface{}, baseFilters map[string]string) (ChangeProductToPriceListResponseBulk, error) {
+	var bulkResp ChangeProductToPriceListResponseBulk
+
+	if len(bulkRequest) > common2.MaxBulkRequestsCount {
+		return bulkResp, fmt.Errorf("cannot add more than %d products to price list in one bulk request", common2.MaxBulkRequestsCount)
+	}
+
+	bulkInputs := make([]common.BulkInput, 0, len(bulkRequest))
+	for _, prodPrice := range bulkRequest {
+		_, isEditMode := prodPrice["priceListProductID"]
+		methodName := "addProductToPriceList"
+		if isEditMode {
+			methodName = "editProductInPriceList"
+		}
+		bulkInputs = append(bulkInputs, common.BulkInput{
+			MethodName: methodName,
+			Filters:    prodPrice,
+		})
+	}
+
+	resp, err := cli.SendRequestBulk(ctx, bulkInputs, baseFilters)
+	if err != nil {
+		return bulkResp, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return bulkResp, err
+	}
+
+	if err := json.Unmarshal(body, &bulkResp); err != nil {
+		return bulkResp, fmt.Errorf("ERPLY API: failed to unmarshal ChangeProductToPriceListBulk from '%s': %v", string(body), err)
+	}
+
+	if !common.IsJSONResponseOK(&bulkResp.Status) {
+		return bulkResp, erro.NewErplyError(bulkResp.Status.ErrorCode.String(), bulkResp.Status.Request+": "+bulkResp.Status.ResponseStatus)
+	}
+
+	for _, bulkRespItem := range bulkResp.BulkItems {
+		if !common.IsJSONResponseOK(&bulkRespItem.Status.Status) {
+			return bulkResp, erro.NewErplyError(
+				bulkRespItem.Status.ErrorCode.String(),
+				fmt.Sprintf("%+v", bulkRespItem.Status),
+			)
+		}
+	}
+
+	return bulkResp, nil
+}
+
+func (cli *Client) DeleteProductsFromPriceList(ctx context.Context, filters map[string]string) (*DeleteProductsFromPriceListResult, error) {
+	resp, err := cli.SendRequest(ctx, "deleteProductInPriceList", filters)
+	if err != nil {
+		return nil, erro.NewFromError("deleteProductInPriceList request failed", err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &DeleteProductsFromPriceListResponse{}
+	if err := json.Unmarshal(body, &res); err != nil {
+		return nil, fmt.Errorf("ERPLY API: failed to unmarshal DeleteProductsFromPriceListResponse from '%s': %v", string(body), err)
+	}
+
+	if !common.IsJSONResponseOK(&res.Status) {
+		return nil, erro.NewFromResponseStatus(&res.Status)
+	}
+
+	if len(res.DeleteProductsFromPriceListResults) == 0 {
+		return nil, nil
+	}
+
+	return &res.DeleteProductsFromPriceListResults[0], nil
+}
+
+
+func (cli *Client) DeleteProductsFromPriceListBulk(
+	ctx context.Context,
+	bulkRequest []map[string]interface{}, baseFilters map[string]string,
+) (DeleteProductsFromPriceListResponseBulk, error) {
+	var bulkResp DeleteProductsFromPriceListResponseBulk
+
+	if len(bulkRequest) > common2.MaxBulkRequestsCount {
+		return bulkResp, fmt.Errorf("cannot delete more than %d products from price list in one bulk request", common2.MaxBulkRequestsCount)
+	}
+
+	bulkInputs := make([]common.BulkInput, 0, len(bulkRequest))
+	for _, bulkInput := range bulkRequest {
+		bulkInputs = append(bulkInputs, common.BulkInput{
+			MethodName: "deleteProductInPriceList",
+			Filters:    bulkInput,
+		})
+	}
+
+	resp, err := cli.SendRequestBulk(ctx, bulkInputs, baseFilters)
+	if err != nil {
+		return bulkResp, err
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return bulkResp, err
+	}
+
+	if err := json.Unmarshal(body, &bulkResp); err != nil {
+		return bulkResp, fmt.Errorf("ERPLY API: failed to unmarshal DeleteProductsFromPriceListResponseBulk from '%s': %v", string(body), err)
+	}
+
+	if !common.IsJSONResponseOK(&bulkResp.Status) {
+		return bulkResp, erro.NewErplyError(bulkResp.Status.ErrorCode.String(), bulkResp.Status.Request+": "+bulkResp.Status.ResponseStatus)
+	}
+
+	for _, bulkRespItem := range bulkResp.BulkItems {
+		if !common.IsJSONResponseOK(&bulkRespItem.Status.Status) {
+			return bulkResp, erro.NewErplyError(
+				bulkRespItem.Status.ErrorCode.String(),
+				fmt.Sprintf("%+v", bulkRespItem.Status),
+			)
+		}
+	}
+
+	return bulkResp, nil
+}
