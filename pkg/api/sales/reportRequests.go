4fe3ba81bf8276f164739e53f9f7bb05646b94a5
Nagy <Nagy@shopz.com>
2020-10-16 16:23:52 +0300

    Return whole struct fir getSalesReport

--- pkg/api/sales/reportRequests.go
+++ pkg/api/sales/reportRequests.go
@@ -9,25 +9,25 @@ import (
 	"net/http"
 )
 
-func (cli *Client) GetSalesReport(ctx context.Context, filters map[string]string)(string, error) {
+func (cli *Client) GetSalesReport(ctx context.Context, filters map[string]string)(*GetSalesReport, error) {
 	var salesReportResp GetSalesReport
 	resp, err := cli.SendRequest(ctx, "getSalesReport", filters)
 	if err != nil {
-		return "", erro.NewFromError("getSalesReport: error sending request", err)
+		return nil, erro.NewFromError("getSalesReport: error sending request", err)
 	}
 	if resp.StatusCode != http.StatusOK {
-		return "", erro.NewFromError(fmt.Sprintf("getSalesReport: bad response status code: %d", resp.StatusCode), nil)
+		return nil, erro.NewFromError(fmt.Sprintf("getSalesReport: bad response status code: %d", resp.StatusCode), nil)
 	}
 
 	if err := json.NewDecoder(resp.Body).Decode(&salesReportResp); err != nil {
-		return "", erro.NewFromError("getSalesReport: unmarshaling response failed", err)
+		return nil, erro.NewFromError("getSalesReport: unmarshaling response failed", err)
 	}
 	if !common.IsJSONResponseOK(&salesReportResp.Status) {
-		return "", erro.NewErplyError(salesReportResp.Status.ErrorCode.String(), salesReportResp.Status.Request+": "+salesReportResp.Status.ResponseStatus)
+		return &salesReportResp, erro.NewErplyError(salesReportResp.Status.ErrorCode.String(), salesReportResp.Status.Request+": "+salesReportResp.Status.ResponseStatus)
 	}
 	if len(salesReportResp.Records) < 1 {
-		return "", erro.NewFromError("getSalesReport: no records in response", nil)
+		return &salesReportResp, erro.NewFromError("getSalesReport: no records in response", nil)
 	}
 
-	return salesReportResp.Records[0].ReportLink, nil
+	return &salesReportResp, nil
 }
